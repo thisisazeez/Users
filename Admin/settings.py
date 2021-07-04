5503a727a570a75c0a78635ed16a6bd12154b083
thisisazeez <imfostersyndrome@gmail.com>
2021-07-04 21:05:43 +0100

    some issues are fixed

--- Admin/settings.py
+++ Admin/settings.py
@@ -132,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'Storage.myUser'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
